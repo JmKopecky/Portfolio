f046adb0a462da3a0cf75cea163d3da2a5d5b26d
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-11-09 12:00:04 -0600

    Created header.

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,18 @@
+function headerButtonClick() {
+    let headerButton = document.getElementById("header-show-button");
+    if (headerButton.getAttribute("data-headeropen") === "true") {
+        headerButton.setAttribute("data-headeropen", "false");
+        for (const link of document.getElementById("header-right").getElementsByTagName("a")) {
+            if (!link.classList.contains("header-no-remove")) {
+                link.setAttribute("style", "display: none;");
+            }
+        }
+    } else {
+        headerButton.setAttribute("data-headeropen", "true");
+        for (const link of document.getElementById("header-right").getElementsByTagName("a")) {
+            if (!link.classList.contains("header-no-remove")) {
+                link.setAttribute("style", "display: block;");
+            }
+        }
+    }
+}
