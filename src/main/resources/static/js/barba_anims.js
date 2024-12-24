afb20c9a7308cf70b58fd619b83f555534f1823f
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-12-24 14:48:00 -0600

    Began work on design wireframe, home page hero section.

--- src/main/resources/static/js/barba_anims.js
+++ src/main/resources/static/js/barba_anims.js
@@ -1,15 +1,20 @@
 barba.init({
     transitions: [{
         name: 'main',
+        sync: true,
         leave(data) {
             return gsap.to(data.current.container, {
-                opacity: 0
+                opacity: 0, duration: 0.25, ease: "power1.inout"
             });
         },
         enter(data) {
-            return gsap.from(data.next.container, {
+            gsap.set(data.next.container, {
                 opacity: 0
+            })
+            gsap.to(data.next.container, {
+                opacity: 1, duration: 0.25, ease: "power1.inout", delay: 0.25
             });
+            return false;
         }
     }]
 });
