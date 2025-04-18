6b651b37f7b4c6b73142c2a7cbacd9b70014cc54
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2025-04-17 20:22:11 -0500

    Added content to the tagline, about, and hobbies/interests section. Let Mendicant Bias into the terminal by accident.

--- readme.md
+++ readme.md
@@ -2,3 +2,8 @@
 * Barba fade animation, subtle
 * No animations except on interactions
 * Gradiant background
+
+# Features
+* Writeup content for the home page
+* Resume page (include more about awards, schooling, etc)
+* Skip to projects button on the home page that scrolls past the about section?
--- src/main/resources/static/js/barba_anims.js
+++ src/main/resources/static/js/barba_anims.js
@@ -1,3 +1,7 @@
+//fun console message on js load initially
+console.log("JK001: Booting...");
+
+
 barba.init({
     preventRunning: true,
     cacheIgnore: true,
@@ -26,20 +30,38 @@ barba.init({
 
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
+
+    //fun console greeting on DOM load
+    console.log("JK001: Boot complete")
+    console.log("JK001: Welcome to console:jkopecky.dev")
+    console.log("JK001: If all goes to plan, there shouldn't be anything worrying here")
+    console.log("JK001: Which means there should be at least five errors, natura-")
+    setTimeout(() => {
+        console.log("  -- Session Hijacked --")
+        setTimeout(() => {
+            console.log("Ghost714: I see you, Reclaimer.")
+        }, 500);
+    }, 500);
+
+
+
     if (history.scrollRestoration) {
         history.scrollRestoration = 'manual';
     }
+    hljs.highlightAll();
+
 
     barba.hooks.enter((data) => {
         lenis.stop();
+        hljs.highlightAll();
+        console.log(2);
     })
 
     barba.hooks.after((data) => {
         window.scrollTo(0,0);
-        lenis.start();
-        lenis.resize();
+        hljs.highlightAll();
+        console.log(3);
 
         if ($("#edit-project-page") !== undefined) {
             editProjectInit();
